70dbf06cd9229dbc875f507e8b70341d3a251f0b
Andrew Davisson <akdavisson4@gmail.com>
2019-06-13 15:31:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,6 +14,7 @@ class Artist
   end
   
   def save
+    match = false
     if @@all.length <= 0
       @@all << self
     else
@@ -21,6 +22,9 @@ class Artist
         if item.name == self.name
           match = true
         end
+      end
+    end
+    
   end
   
   def self.all
