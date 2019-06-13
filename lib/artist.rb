8029578d2d75065904258245d01303001ca94e20
Andrew Davisson <akdavisson4@gmail.com>
2019-06-13 14:38:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,14 @@
 class Artist
   
   attr_accessor :name, :songs
+  @@artists = []
   
   def initialize(name)
     @name = name
     @songs = []
   end
+  
+  def find_or_create_by_name(name)
+    
   
 end
