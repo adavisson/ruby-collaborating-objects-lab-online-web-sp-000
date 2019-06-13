80ec37c573f6cd44fe708fb57af90d960e215170
Andrew Davisson <akdavisson4@gmail.com>
2019-06-13 15:05:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
   
   def self.new_by_filename(filename)
     info = filename.split(" - ")
-    binding.pry
+    #binding.pry
     song = self.new(info[1])
     @artist = Artist.find_or_create_by_name(info[0])
   end
