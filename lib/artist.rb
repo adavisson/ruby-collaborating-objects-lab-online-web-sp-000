5135d3e725036c674c42edc207e5390b34f54200
Andrew Davisson <akdavisson4@gmail.com>
2019-06-13 15:17:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,15 +23,11 @@ class Artist
   
   def self.find_or_create_by_name(name)
     match = nil
-    if @@all.length <= 0
-      match = self.new(name)
-    else
-      @@all.each do |artist|
-        if artist.name == name
-          match = artist
-        else
-          match = self.new(name)
-        end
+    @@all.each do |artist|
+      if artist.name == name
+        match = artist
+      else
+        match = self.new(name)
       end
     end
     match
